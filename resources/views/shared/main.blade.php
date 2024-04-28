f3622084b6e9e6553e07801a26a19387ffeb0954
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2024-04-29 02:06:29 +0700

    add mix

---
+++ resources/views/shared/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>BRI Console</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}"></script>
+</head>
+
+<body class="antialiased">
+    <div class="tes">ADRIAN</div>
+    <div class="dua">sania</div>
+
+</body>
+
+</html>
