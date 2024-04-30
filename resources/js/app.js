847fd3171e10b3d83f38cc40e73fbf286f59879b
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2024-04-30 16:58:56 +0700

    add antrian history and auto scroll produk

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,5 @@ require('./bootstrap');
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
+
+require('./tabl_auto_scroll_down');
--- resources/views/shared/main.blade.php
+++ resources/views/shared/main.blade.php
@@ -9,12 +9,13 @@
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}"></script>
+    {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> --}}
 </head>
 
-<body class="d-flex flex-column min-vh-100">
-    <div class="bg-primary-app">
-        <div class="row">
-            <div class="col-md-7">
+<body class="d-flex flex-column min-vh-100 bg-primary-app">
+    <div class="container-fluid">
+        <div class="row ">
+            <div class="col-md-7 container-fluid">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="video-container rounded">
@@ -43,8 +44,8 @@
                             <div class="row">
                                 <div class="col-6">
                                     <div>
-                                        <h2 class="fw-bolder text-white title-info" style="margin-left: 4%; "> BRITAMA
-                                            (RP) </h2>
+                                        <h1 class="fw-bolder text-center text-white title-info"> BRITAMA
+                                            (RP) </h1>
                                     </div>
 
                                 </div>
@@ -55,34 +56,27 @@
                                 border-left:1px solid rgb(255, 255, 255);
                                 border-top-left-radius: 500px;
                                 margin-top:5px;">
-                                        <h2 class="fw-bolder text-center title-info" style="color: #faa901!important;">
+                                        <h1 class="fw-bolder text-center title-info" style="color: #faa901!important;">
                                             TARIF SUKU
-                                            BUNGA (% PA) </h2>
+                                            BUNGA (% PA) </h1>
                                     </div>
 
                                 </div>
 
                                 <div class="col-12 text-center">
-                                    <div class="table-info">
+                                    <div class="table-info parent-table-product table-auto-scroll">
                                         <table class="table my-table-product">
                                             <tbody>
-                                                <tr>
-                                                    <td>
-                                                        <h3>tes</h3>
-                                                    </td>
-                                                    <td>
-                                                        <h3>tes</h3>
-                                                    </td>
-                                                </tr>
-
-                                                <tr>
-                                                    <td>
-                                                        <h3>tes</h3>
-                                                    </td>
-                                                    <td>
-                                                        <h3>tes</h3>
-                                                    </td>
-                                                </tr>
+                                                @for ($i = 0; $i < 20; $i++)
+                                                    <tr>
+                                                        <td>
+                                                            <h2>tes {{ $i }}</h2>
+                                                        </td>
+                                                        <td>
+                                                            <h2>{{ $i }} %%</h2>
+                                                        </td>
+                                                    </tr>
+                                                @endfor
                                             </tbody>
                                         </table>
                                     </div>
@@ -93,52 +87,81 @@
                 </div>
             </div>
 
-            <div class="col-5">
-                <div class="row right-bar">
-                    <div class="col-md-12">
+            <div class="col-5 container-fluid">
+                <div class="row right-bar position-relative">
+                    <div class="col-md-12 container-fluid">
                         <div class="text-center">
                             <img src="{{ asset('images/logo_bri_2.png') }}" alt="Logo BRI" class="logo">
                             <h1 class="text-white">BRI UNIT COLOMADU</h1>
                         </div>
                     </div>
 
-                    <div class="col-md-12">
+                    <div class="col-md-12 container-fluid">
                         <div class="row counter-parent">
                             <div class="col-5 text-center counter-div-left">
-                                <span class="counter-left counter-color">1</span>
+                                <span class="counter-left counter-color counter-number">1</span>
                             </div>
                             <div class="col-7 text-center counter-div-right">
-                                <span class="counter-right counter-color">A05</span>
+                                <span class="counter-right counter-color counter-number">A05</span>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-md-12">
+                    <div class="col-md-12 container-fluid">
                         <div class="row counter-parent right-bar-title">
                             <div class="col-5 text-center title-div-left">
-                                <span class="">LOKET</span>
+                                <span>LOKET</span>
                             </div>
                             <div class="col-7 text-center title-div-right">
-                                <span class="">NO TIKET</span>
+                                <span>NO TIKET</span>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-md-12">
+                    <div class="col-md-12 container-fluid">
+                        <div class="row counter-parent right-bar-counter">
+                            <div class="col-5 text-center right-bar-counter-left">
+                                <div class="parent-right-bar-counter-left">
+                                    <h1 class="text-white counter-number">02</h1>
+                                </div>
+                            </div>
+                            <div class="col-7 text-center">
+                                <div class="parent-right-bar-counter-right">
+                                    <h1 class="text-white right-bar-counter-left counter-number">A001</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row counter-parent right-bar-counter">
+                            <div class="col-5 text-center right-bar-counter-left">
+                                <div class="parent-right-bar-counter-left rounded">
+                                    <h1 class="text-white counter-number">03</h1>
+                                </div>
+                            </div>
+                            <div class="col-7 text-center">
+                                <div class="parent-right-bar-counter-right rounded">
+                                    <h1 class="text-white right-bar-counter-left counter-number">A003</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- INI KETIKA ADA ANTRIAN MASUK
+                    <div class="col-md-12 container-fluid">
                         <div class="row counter-parent right-bar-caller">
                             <div class="col-12 caller-div position-relative">
-                                <div class="col-12 position-absolute top-0 start-0" style="padding: 10px">
+                                <div class="col-12" style="padding: 10px">
                                     <p class="display-5">Antrian Nomor</p>
                                     <span class="display-1 counter-color">A001</span>
                                 </div>
 
-                                <div class="col-12 position-absolute bottom-0 start-0" style="padding: 10px">
+                                <div class="col-12" style="padding: 10px">
                                     <p class="display-5">Silahkan Menuju Ke</p>
                                     <span class="display-1 counter-color">02</span>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    END ANTRIAN MASUK --}}
 
                 </div>
             </div>
@@ -150,7 +173,7 @@
 
     <script>
         $(document).ready(function() {
-            console.log($("#myVideo").prop("volume", 10));
+
         });
     </script>
 </body>
