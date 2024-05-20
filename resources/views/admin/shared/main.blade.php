9fce4108f037715ada9d59087177081e8a05dd3d
Adrian R <mochamad.rahmandanu@mekari.com>
2024-05-20 17:09:21 +0700

    add map

--- resources/views/admin/shared/main.blade.php
+++ resources/views/admin/shared/main.blade.php
@@ -25,14 +25,17 @@
     <link href="{{ asset('vendor/quill/quill.bubble.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet">
     <link href="{{ asset('vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <script src="{{ asset('vendor/tinymce/tinymce.js') }}"></script>
+
     <!-- Template Main CSS File -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('vendor/leaflet/leaflet.js') }}"></script>
+    {{-- <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script> --}}
+    <link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet" />
 </head>
 
 <body>
+    <div id="osm-map" style="height: 500px;"></div>
 
     @include('admin.shared.header')
 
@@ -49,12 +52,45 @@
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
+    <script>
+        $(document).ready(function() {
+            // Where you want to render the map.
+            var element = document.getElementById('osm-map');
+
+            // Create Leaflet map on map element.
+            var map = L.map(element);
+
+            // Add OSM tile layer to the Leaflet map.
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+
+            // Target's GPS coordinates.
+            data = [
+                ['-7.7627747', '110.4142924'],
+                ['-7.7613647', '110.4204982'],
+                ['-7.7605568', '110.4181808'],
+            ]
+            $.each(data, function(indexInArray, valueOfElement) {
+                var target = L.latLng(valueOfElement[0], valueOfElement[1]);
+                // Set map's center to target with zoom 14.
+                map.setView(target, 15);
+                // Place a marker on the same location.
+                L.marker(target).addTo(map).bindPopup(indexInArray + 'A pretty CSS popup.<br> Easily customizable.')
+                    .openPopup();
+            });
+
+        });
+    </script>
+
     <script src="{{ asset('vendor/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ asset('vendor/chart.js/chart.umd.js') }}"></script>
     <script src="{{ asset('vendor/echarts/echarts.min.js') }}"></script>
     <script src="{{ asset('vendor/quill/quill.js') }}"></script>
     <script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
     <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('vendor/tinymce/tinymce.js') }}"></script>
 </body>
 
 </html>
