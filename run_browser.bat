8c094146135a9e518090cafd9a36bdb535d219bd
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-02-04 21:52:40 +0700

    adjust

--- run_browser.bat
+++ run_browser.bat
@@ -1,6 +1,8 @@
 
-start msedge --app=http://localhost/antrian_bri_console/kios --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --window-position=0,0 --new-window --user-data-dir=%TEMP%/monitor1
-start msedge --app=http://localhost/antrian_bri_console/ --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --window-position=1920,0 --new-window --user-data-dir=%TEMP%/monitor0 --edge-kiosk-type=fullscreen
+
+start msedge --app=http://localhost/antrian_bri_console/kios --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --new-window --user-data-dir=%TEMP%/monitor1 --edge-kiosk-type=fullscreen
+start msedge --app=http://localhost/antrian_bri_console/ --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --new-window --user-data-dir=%TEMP%/monitor0 --edge-kiosk-type=fullscreen
+
 
 @REM start "" "C:\Program Files\Mozilla Firefox\firefox.exe" --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=0,0 --private-window localhost/antrian_bri_console
 @REM start "" "C:\Program Files\Mozilla Firefox\firefox.exe" --kiosk --user-data-dir=%TEMP%/monitor0 --window-position=1920,0 --private-window localhost/antrian_bri_console/kios
