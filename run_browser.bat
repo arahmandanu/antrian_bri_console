e581f928bcd2e74e6c86f6c3bd52e054c0aeb8af
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-02-15 16:32:10 +0700

    asd

--- run_browser.bat
+++ run_browser.bat
@@ -10,3 +10,6 @@ start msedge --app=http://localhost/antrian_bri_console/ --kiosk --no-first-run 
 
 @REM start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --kiosk --user-data-dir=%TEMP%/monitor0 --window-position=1920,0 --private-window localhost/antrian_bri_console/kios
 @REM start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=0,0 --private-window localhost/antrian_bri_console
+
+@REM start msedge --app=http://localhost/antrian_bri_console/kios --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --new-window --user-data-dir=%TEMP%/monitor0 --edge-kiosk-type=fullscreen
+@REM start msedge --app=http://localhost/antrian_bri_console/ --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --user-data-dir=%TEMP%/monitor1 --edge-kiosk-type=fullscreen
