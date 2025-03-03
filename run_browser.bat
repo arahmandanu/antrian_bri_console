b5e4ccc9de72ecf2670e4f28cfb0c2834c0881f5
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-03-03 20:30:07 +0700

    ew

--- run_browser.bat
+++ run_browser.bat
@@ -1,11 +1,11 @@
 
 
-start msedge --app=http://localhost/antrian_bri_console/kios --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --new-window --user-data-dir=%TEMP%/monitor1 --edge-kiosk-type=fullscreen
-start msedge --app=http://localhost/antrian_bri_console/ --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --new-window --user-data-dir=%TEMP%/monitor0 --edge-kiosk-type=fullscreen
+@REM start msedge --app=http://localhost/antrian_bri_console/kios --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --new-window --user-data-dir=%TEMP%/monitor1 --edge-kiosk-type=fullscreen
+@REM start msedge --app=http://localhost/antrian_bri_console/ --kiosk --no-first-run --autoplay-policy=no-user-gesture-required --new-window --user-data-dir=%TEMP%/monitor0 --edge-kiosk-type=fullscreen
 
 
-@REM start "" "C:\Program Files\Mozilla Firefox\firefox.exe" --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=0,0 --private-window localhost/antrian_bri_console
-@REM start "" "C:\Program Files\Mozilla Firefox\firefox.exe" --kiosk --user-data-dir=%TEMP%/monitor0 --window-position=1920,0 --private-window localhost/antrian_bri_console/kios
+start firefox --kiosk --user-data-dir=%TEMP%/monitor1 --window-position=0,1920 -new-tab localhost/antrian_bri_console
+start firefox --kiosk --user-data-dir=%TEMP%/monitor0 --window-position=1920,0 -new-tab localhost/antrian_bri_console/kios
 
 
 @REM start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --kiosk --user-data-dir=%TEMP%/monitor0 --window-position=1920,0 --private-window localhost/antrian_bri_console/kios
