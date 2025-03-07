b9a71e76792961896aaa24480522b189a6a8359d
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-03-07 16:50:55 +0700

    adjust delete

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -150,6 +150,7 @@ class MainController extends Controller
         // exec('start /B tasklist /nh /fi "ImageName eq Console.exe"', $task_list);
         exec('start /B taskkill /IM firefox.exe /F', $task_list);
         exec('start /B taskkill /IM msedge.exe /F', $task_list);
+        exec('start /B taskkill /IM chrome.exe /F', $task_list);
         $message = null;
         $alreadyRun = false;
         $data = [];
